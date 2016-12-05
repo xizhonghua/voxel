5265e93283d67a3f8298431fbd46dbd5a12dcf29
Zhonghua Xi <xizhonghua@gmail.com>
2016-12-05 15:11:16 -0500

    update

--- main.cpp
+++ main.cpp
@@ -5,51 +5,83 @@
 #define TINYOBJLOADER_IMPLEMENTATION // define this in only *one* .cc
 #include "tiny_obj_loader.h"
 
-void voxelization(const std::string& input_path) {
-  std::string inputfile = input_path;
-  tinyobj::attrib_t attrib;
+#define VOXELIZER_IMPLEMENTATION
+#include "voxelizer.h"
+
+void voxelization(const std::string& input_path, const double res, const double precision) {
   std::vector<tinyobj::shape_t> shapes;
   std::vector<tinyobj::material_t> materials;
-
   std::string err;
-  bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, inputfile.c_str());
-
-  if (!err.empty()) { // `err` may contain warning message.
+  bool ret = tinyobj::LoadObj(shapes, materials, err, input_path.c_str(), NULL);
+  
+  if (!err.empty()) {
     std::cerr << err << std::endl;
   }
-
+  
   if (!ret) {
-    exit(1);
+    return exit(-1);
   }
-
-  // Loop over shapes
-  for (size_t s = 0; s < shapes.size(); s++) {
-    // Loop over faces(polygon)
-    size_t index_offset = 0;
-    for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
-      int fv = shapes[s].mesh.num_face_vertices[f];
-
-      // Loop over vertices in the face.
-      for (size_t v = 0; v < fv; v++) {
-        // access to vertex
-        tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
-        float vx = attrib.vertices[3*idx.vertex_index+0];
-        float vy = attrib.vertices[3*idx.vertex_index+1];
-        float vz = attrib.vertices[3*idx.vertex_index+2];
-      }
-      index_offset += fv;
-
-      // per-face material
-      shapes[s].mesh.material_ids[f];
+  
+  std::ofstream file("mesh_voxelized.obj");
+  
+  for (size_t i = 0; i < shapes.size(); i++) {
+    vx_mesh_t* mesh;
+    vx_mesh_t* result;
+    
+    mesh = vx_mesh_alloc(shapes[i].mesh.positions.size(), shapes[i].mesh.indices.size());
+    
+    for (size_t f = 0; f < shapes[i].mesh.indices.size(); f++) {
+      mesh->indices[f] = shapes[i].mesh.indices[f];
     }
+    for (size_t v = 0; v < shapes[i].mesh.positions.size() / 3; v++) {
+      mesh->vertices[v].x = shapes[i].mesh.positions[3*v+0];
+      mesh->vertices[v].y = shapes[i].mesh.positions[3*v+1];
+      mesh->vertices[v].z = shapes[i].mesh.positions[3*v+2];
+    }
+    
+    result = vx_voxelize(mesh, res, res, res, precision);
+    
+    printf("Number of vertices: %ld\n", result->nvertices);
+    printf("Number of indices: %ld\n", result->nindices);
+    
+    if (file.is_open()) {
+      file << " " << "\n";
+      for (int i = 0; i < result->nvertices; ++i) {
+        file << "v " << result->vertices[i].x << " "
+        <<  result->vertices[i].y << " "
+        <<  result->vertices[i].z << "\n";
+      }
+      for (int i = 0; i < result->nnormals; ++i) {
+        file << "vn " << result->normals[i].x << " "
+        << result->normals[i].y << " "
+        << result->normals[i].z << "\n";
+      }
+      
+      for (int i = 0; i < result->nindices; i += 3) {
+        file << "f " << result->indices[i] + 1 << "//"
+        << result->normalindices[i] + 1;
+        file << " ";
+        file << result->indices[i+1] + 1 << "//"
+        << result->normalindices[i+1] + 1;
+        file << " ";
+        file << result->indices[i+2] + 1 << "//"
+        << result->normalindices[i+2] + 1 << "\n";
+      }
+    }
+    
+    vx_mesh_free(result);
+    vx_mesh_free(mesh);
   }
 }
 
 int main(int argc, char** argv) {
-  if(argc<2) {
-    std::cerr << "Usage: "<< argv[0] << " *.obj" << std::endl;
+  if(argc<4) {
+    std::cerr << "Usage: "<< argv[0] << " *.obj res precision" << std::endl;
     exit(1);
   }
+  
+  double res = std::atof(argv[2]);
+  double precision = std::atof(argv[3]);
 
-  voxelization(std::string(argv[1]));
+  voxelization(std::string(argv[1]), res, precision);
 }
